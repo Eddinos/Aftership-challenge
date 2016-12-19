a3545dc9d104ff43fd1d4171b282c0f3f650e390
Eddine <eddine.djerboua@gmail.com>
2016-12-19 11:19:13 +0100

    https for rate requests

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: "36c79764e2ba4d61aeeb3b3364c92273",
   currenciesAPI: "https://openexchangerates.org/api/currencies.json",
   latestAPI: "https://openexchangerates.org/api/latest.json",
-  latestRatesAPI: "http://api.fixer.io/latest",
-  ratesAPI: "http://api.fixer.io"
+  latestRatesAPI: "https://api.fixer.io/latest",
+  ratesAPI: "https://api.fixer.io"
 }
